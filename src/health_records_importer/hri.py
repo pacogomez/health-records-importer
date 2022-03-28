3d34b137ffc1d53e3764958e8a0b9e28682202d4
Paco Gómez <contact@pacogomez.com>
2022-03-28 10:25:25 -0500

    initial

---
+++ src/health_records_importer/hri.py
@@ -0,0 +1,28 @@
+__copyright__ = 'Copyright (C) 2021 Paco Gomez'
+__license__ = 'GNU GPLv3'
+
+import click
+import sys
+
+@click.group()
+@click.option('-f', '--filename', type=click.Path(exists=True))
+@click.option('-c', '--config', type=click.Path(exists=True))
+@click.option('-q', '--quiet', is_flag=True, default=False)
+@click.pass_context
+def cli(ctx, filename, config, quiet):
+    ctx.ensure_object(dict)
+    ctx.obj['filename'] = filename
+    if config is not None:
+        with open(config) as file:
+            ctx.obj['config'] = yaml.load(file, Loader=yaml.FullLoader)
+    ctx.obj['quiet'] = quiet
+
+@click.command('version')
+@click.pass_context
+def version_cmd(ctx):
+    print('not implemented')
+
+cli.add_command(version_cmd)
+
+if __name__ == 'health_records_importer.hri':
+    cli(obj={}, auto_envvar_prefix='HRI')
